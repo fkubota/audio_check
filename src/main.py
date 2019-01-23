ed84f798a65297b1b2e69854dd5448cc586b5aba
fkubota_office <fumihiro.kubota@hmcom.co.jp>
2019-01-23 19:13:25 +0900

    音を途中から再生できるようにした。
    ogg ファイルを使うのがミソ

--- src/main.py
+++ src/main.py
@@ -13,6 +13,8 @@ import pyqtgraph as pg
 import pickle
 import pygame
 import time
+import pydub
+import os
 
 class audio_check(QG.QMainWindow):
     def __init__(self, parent = None):
@@ -45,13 +47,16 @@ class audio_check(QG.QMainWindow):
         self.w_plot_wav.setFixedHeight(200)
         self.w_plot_wav.setBackground('#FFFFFF00')
         self.p_wav = self.w_plot_wav.addPlot()
-        self.p_wav.setLabel('bottom', 'Time', 'hour')
+        self.p_wav.setLabel('bottom', 'Time', 'sec')
         self.p_wav.showGrid(x=True, y=True, alpha=0.7)
         self.curve_wav = self.p_wav.plot(pen=(255, 0, 0, 50))
         self.infline_wav = pg.InfiniteLine(pen=(0, 0, 0), movable=True, hoverPen=(0, 0, 255))
         self.infline_wav.sigPositionChangeFinished.connect(self.infline_changed)
         self.p_wav.addItem(self.infline_wav)
 
+        # region
+        # self.region = pg.LinearRegionItem()
+        # self.p_wav.addItem(self.region)
 
         # graph_feat
         self.w_plot_feat = pg.GraphicsWindow()
@@ -62,7 +67,7 @@ class audio_check(QG.QMainWindow):
         self.infline_featV = []
         for idx in range(34):
             self.p_featV.append(self.w_plot_feat.addPlot())
-            self.p_featV[idx].setLabel('bottom', 'Time', 'hour')
+            self.p_featV[idx].setLabel('bottom', 'Time', 'sec')
             self.p_featV[idx].showGrid(x=True, y=True, alpha=0.7)
             # self.p_featV[idx].setXRange(0, 200)
             self.curve_featV.append(self.p_featV[idx].plot(pen=(255, 0, 0, 100)))
@@ -71,14 +76,6 @@ class audio_check(QG.QMainWindow):
             self.p_featV[idx].addItem(self.infline_featV[idx])
             self.w_plot_feat.nextRow()
 
-        # self.p_feat = self.w_plot_feat.addPlot()
-        # self.p_feat.setLabel('bottom', 'Time', 'hour')
-        # self.p_feat.showGrid(x=True, y=True, alpha=0.7)
-        # self.curve_feat = self.p_feat.plot(pen=(255, 0, 0, 50))
-        # self.infline_feat = pg.InfiniteLine(pen=(0, 0, 0), movable=True, hoverPen=(0, 0, 255))
-        # self.p_feat.addItem(self.infline_feat)
-        # self.p_feat.setYRange(-40000,40000)
-        # self.p_feat.setXRange(0, 2000000)
         self.scroll.setWidget(self.w_plot_feat)
         self.hbox_plot_feat = QG.QHBoxLayout()
         self.hbox_plot_feat.addWidget(self.scroll)
@@ -113,10 +110,23 @@ class audio_check(QG.QMainWindow):
 
 
     def play(self):
-        filename = self.wav_path
+        self.btn_play_stop.setEnabled(False)
+        self.btn_play_stop.setText('loading')
+        filename_wav = self.wav_path
+        dir = os.path.dirname(filename_wav)
+        base_name = os.path.basename(filename_wav).split('.')[0]
+        # filename_mp3 = dir+'/'+base_name+".mp3"
+        filename_ogg = dir + '/' + base_name+ '.ogg'
+        if os.path.isfile(filename_ogg)==False:
+            sound = pydub.AudioSegment.from_wav(filename_wav)
+            sound.export(filename_ogg, format="ogg")
+        # sound.export("output.mp3", format="mp3")
+        self.btn_play_stop.setEnabled(True)
+        self.btn_play_stop.setText('Play')
+
 
         pygame.mixer.init()
-        pygame.mixer.music.load(filename) #音源を読み込み
+        pygame.mixer.music.load(filename_ogg) #音源を読み込み
         # mp3_length = mp3(filename).info.length #音源の長さ取得
         # pygame.mixer.music.play(1) #再生開始。1の部分を変えるとn回再生(その場合は次の行の秒数も×nすること)
         # time.sleep(mp3_length + 0.25) #再生開始後、音源の長さだけ待つ(0.25待つのは誤差解消)
@@ -128,13 +138,14 @@ class audio_check(QG.QMainWindow):
         # self.time = self.infline_pos
         self.a = time.time()
         def change_infline():
-            print('music: ' + str(pygame.mixer.music.get_pos()))
+            # print('music: ' + str(pygame.mixer.music.get_pos()))
             # self.infline_wav.setPos(self.time*44100/4)
             self.infline_wav.setPos(self.infline_pos)
             for idx in range(34):
                 self.infline_featV[idx].setPos(self.infline_pos)
 
-            self.infline_pos += 1
+            self.infline_pos += 0.25
+            print(self.infline_pos)
             self.now = time.time()- self.a
             # print(self.now)
 
@@ -146,25 +157,31 @@ class audio_check(QG.QMainWindow):
 
     def thread(self):
         if pygame.mixer.music.get_busy() == False:
+            print('start play')
             pygame.mixer.music.play()
         # print('music: ' + str(pygame.mixer.music.get_pos()))
         self.isStop = int((self.isStop+1)%2)
         if self.isStop:
+            pygame.mixer.music.stop()
             self.timer.stop()
             # self.timer.deleteLater()
             # self.timer.destroyed()
             self.btn_play_stop.setText('Play')
-            # pygame.mixer.music.pause()
-            pygame.mixer.music.stop()
+            # pygame.mixer.music.stop()
             return
         self.btn_play_stop.setText('Stop')
 
 
         # pygame.mixer.music.play(1) #再生開始。1の部分を変えるとn回再生(その場合は次の行の秒数も×nすること)
-        print(self.infline_pos)
-        # pygame.mixer.music.play(start = self.infline_pos/4*100)
-        pygame.mixer.music.play(start=10)
-        pygame.mixer.music.set_pos()
+        print('inf : '+ str(self.infline_pos))
+        pygame.mixer.music.play(start = self.infline_pos)
+        # pygame.mixer.music.stop()
+        # pygame.mixer.music.set_pos(10)
+        # pygame.mixer.music.play(-1, self.infline_pos/4)
+        # pygame.mixer.music.rewind()
+        # pygame.mixer.music.set_pos(self.infline_pos)
+        # pygame.mixer.music.unpause()
+        # pygame.mixer.music.set_pos()
         # pygame.mixer.music.set_pos(1)
         # pygame.mixer.music.unpause()
 
@@ -178,10 +195,17 @@ class audio_check(QG.QMainWindow):
         self.le0.setText(self.wav_path)
 
         self.wav_file = wave.open(self.wav_path, 'rb')
+        # print(1, self.wav_file.getframerate())
+        # print(2, self.wav_file.getnframes())
         y = self.wav_file.readframes(self.wav_file.getnframes())
+        # print(3, len(y))
         y = np.frombuffer(y, dtype='int16')
-        # y = np.frombuffer(y, dtype='float16')
-        self.curve_wav.setData(y[::int(44100/4)])
+        # print(4, len(y))
+        y = y[::int(44100/4)]
+        # print(5, len(y))
+        x = np.arange(0, len(y))/4
+        self.curve_wav.setData(x, y)
+        self.wav_file.close()
 
         thread_obj2 = threading.Thread(target=self.play)
         thread_obj2.start()
@@ -198,12 +222,14 @@ class audio_check(QG.QMainWindow):
         except:
             pass
 
-        for idx in range(10):
-            self.curve_featV[idx].setData(self.feat[:, idx])
+        for idx in range(34):
+            x = np.arange(0, len(self.feat[:, idx]))/4
+            self.curve_featV[idx].setData(x, self.feat[:, idx])
 
     def infline_changed(self):
         infline = self.sender()
         pos = infline.pos()[0]
+        # print(infline.pos()[0])
         # print(pos)
         self.infline_pos = int(pos)
 
--- src/test.py
+++ src/test.py
@@ -2,13 +2,28 @@
 import pygame
 import time
 
-filename = '../data/test/window_broken.wav' #再生したいmp3ファイル
+# filename = '../data/test/window_broken.wav' #再生したいmp3ファイル
+# filename = '/home/fkubota/Music/futta-lifetime.ogg' #再生したいmp3ファイル
+filename = '/home/fkubota/Project/Yokogawa/徳山防爆エリア201812/data/徳山防爆エリア201812/record_20181221_162632_環境音/20181221_162632_01.ogg'
 # filename = 'eine.mp3' #再生したいmp3ファイル
 # filename = 'window_broken.wav' #再生したいmp3ファイル
 pygame.mixer.init()
 pygame.mixer.music.load(filename) #音源を読み込み
 # mp3_length = mp3(filename).info.length #音源の長さ取得
-pygame.mixer.music.play(1) #再生開始。1の部分を変えるとn回再生(その場合は次の行の秒数も×nすること)
-# time.sleep(mp3_length + 0.25) #再生開始後、音源の長さだけ待つ(0.25待つのは誤差解消)
-time.sleep(5)
-pygame.mixer.music.stop() #音源の長さ待ったら再生停止
+
+i = 0
+while True:
+    print(i)
+    pygame.mixer.music.play(start=0) #再生開始。1の部分を変えるとn回再生(その場合は次の行の秒数も×nすること)
+    time.sleep(3)
+    pygame.mixer.music.stop()
+    pygame.mixer.music.play(start=10+i*1000)
+    # time.sleep(3)
+    # pygame.mixer.music.stop()
+    # time.sleep(1)
+    # pygame.mixer.music.load(filename) #音源を読み込み
+    # pygame.mixer_music.play(1, 3600)
+    # time.sleep(mp3_length + 0.25) #再生開始後、音源の長さだけ待つ(0.25待つのは誤差解消)
+    time.sleep(3)
+    pygame.mixer.music.stop() #音源の長さ待ったら再生停止
+    i += 1
