6b833f3286880d3457fb328b3eb9d67ec9573f52
fkubota_office <fumihiro.kubota@hmcom.co.jp>
2019-01-23 12:22:20 +0900

    アップデート
    現在の問題は、音声を途中から再生できないこと

--- src/main.py
+++ src/main.py
@@ -20,6 +20,9 @@ class audio_check(QG.QMainWindow):
 
         self.resize(600, 600)
 
+        self.isStop = 1
+        self.infline_pos = 0
+
         # widget
         self.w0 = QG.QWidget()
         self.setCentralWidget(self.w0)
@@ -33,8 +36,8 @@ class audio_check(QG.QMainWindow):
         self.btn1 = QG.QPushButton('...')
         self.btn1.setFixedWidth(30)
         self.btn1.clicked.connect(self.get_path_feat)
-        self.btn_play = QG.QPushButton('Play')
-        self.btn_play.clicked.connect(self.thread)
+        self.btn_play_stop = QG.QPushButton('Play')
+        self.btn_play_stop.clicked.connect(self.thread)
         self.scroll = QG.QScrollArea()
 
         # graph_wav
@@ -46,9 +49,8 @@ class audio_check(QG.QMainWindow):
         self.p_wav.showGrid(x=True, y=True, alpha=0.7)
         self.curve_wav = self.p_wav.plot(pen=(255, 0, 0, 50))
         self.infline_wav = pg.InfiniteLine(pen=(0, 0, 0), movable=True, hoverPen=(0, 0, 255))
+        self.infline_wav.sigPositionChangeFinished.connect(self.infline_changed)
         self.p_wav.addItem(self.infline_wav)
-        self.p_wav.setYRange(-40000,40000)
-        self.p_wav.setXRange(0, 2000000)
 
 
         # graph_feat
@@ -62,9 +64,10 @@ class audio_check(QG.QMainWindow):
             self.p_featV.append(self.w_plot_feat.addPlot())
             self.p_featV[idx].setLabel('bottom', 'Time', 'hour')
             self.p_featV[idx].showGrid(x=True, y=True, alpha=0.7)
-            self.p_featV[idx].setXRange(0, 200)
+            # self.p_featV[idx].setXRange(0, 200)
             self.curve_featV.append(self.p_featV[idx].plot(pen=(255, 0, 0, 100)))
             self.infline_featV.append(pg.InfiniteLine(pen=(0, 0, 0), movable=True, hoverPen=(0, 0, 255)))
+            self.infline_featV[idx].sigPositionChangeFinished.connect(self.infline_changed)
             self.p_featV[idx].addItem(self.infline_featV[idx])
             self.w_plot_feat.nextRow()
 
@@ -98,12 +101,15 @@ class audio_check(QG.QMainWindow):
         self.vbox0.addLayout(self.hbox_plot_feat)
         self.vbox0.addLayout(self.hbox1)
         self.vbox0.addLayout(self.hbox2)
-        self.vbox0.addWidget(self.btn_play)
+        self.vbox0.addWidget(self.btn_play_stop)
         self.w0.setLayout(self.vbox0)
 
 
-        self.get_path_wav()
-        self.get_path_feat()
+        # self.get_path_wav()
+        # self.get_path_feat()
+        self.timer()
+        # thread_obj2 = threading.Thread(target=self.play)
+        # thread_obj2.start()
 
 
     def play(self):
@@ -112,52 +118,78 @@ class audio_check(QG.QMainWindow):
         pygame.mixer.init()
         pygame.mixer.music.load(filename) #音源を読み込み
         # mp3_length = mp3(filename).info.length #音源の長さ取得
-        pygame.mixer.music.play(1) #再生開始。1の部分を変えるとn回再生(その場合は次の行の秒数も×nすること)
+        # pygame.mixer.music.play(1) #再生開始。1の部分を変えるとn回再生(その場合は次の行の秒数も×nすること)
         # time.sleep(mp3_length + 0.25) #再生開始後、音源の長さだけ待つ(0.25待つのは誤差解消)
         # time.sleep(5)
         # pygame.mixer.music.stop() #音源の長さ待ったら再生停止
 
 
     def timer(self):
-        self.time = 0
+        # self.time = self.infline_pos
         self.a = time.time()
         def change_infline():
-            self.infline_wav.setPos(self.time*44100/4)
+            print('music: ' + str(pygame.mixer.music.get_pos()))
+            # self.infline_wav.setPos(self.time*44100/4)
+            self.infline_wav.setPos(self.infline_pos)
             for idx in range(34):
-                self.infline_featV[idx].setPos(self.time)
+                self.infline_featV[idx].setPos(self.infline_pos)
 
-            self.time += 1
+            self.infline_pos += 1
             self.now = time.time()- self.a
-            print(self.now)
+            # print(self.now)
 
 
         self.timer = QC.QTimer()
         self.timer.timeout.connect(change_infline)
-        self.timer.start(250)  # 250 ms 毎
+        # self.timer.start(250)  # 250 ms 毎
 
 
     def thread(self):
-        self.timer()
-        thread_obj2 = threading.Thread(target=self.play)
-        thread_obj2.start()
+        if pygame.mixer.music.get_busy() == False:
+            pygame.mixer.music.play()
+        # print('music: ' + str(pygame.mixer.music.get_pos()))
+        self.isStop = int((self.isStop+1)%2)
+        if self.isStop:
+            self.timer.stop()
+            # self.timer.deleteLater()
+            # self.timer.destroyed()
+            self.btn_play_stop.setText('Play')
+            # pygame.mixer.music.pause()
+            pygame.mixer.music.stop()
+            return
+        self.btn_play_stop.setText('Stop')
+
+
+        # pygame.mixer.music.play(1) #再生開始。1の部分を変えるとn回再生(その場合は次の行の秒数も×nすること)
+        print(self.infline_pos)
+        # pygame.mixer.music.play(start = self.infline_pos/4*100)
+        pygame.mixer.music.play(start=10)
+        pygame.mixer.music.set_pos()
+        # pygame.mixer.music.set_pos(1)
+        # pygame.mixer.music.unpause()
+
+        self.timer.start(250)  # 250 ms 毎
+
 
 
     def get_path_wav(self):
-        # self.wav_path = QG.QFileDialog.getOpenFileName(self, 'Get Wav File', '/home/')
-        self.wav_path = '../data/test/window_broken.wav'
+        self.wav_path = QG.QFileDialog.getOpenFileName(self, 'Get Wav File', '/home/')
+        # self.wav_path = '../data/test/window_broken.wav'
         self.le0.setText(self.wav_path)
 
         self.wav_file = wave.open(self.wav_path, 'rb')
         y = self.wav_file.readframes(self.wav_file.getnframes())
         y = np.frombuffer(y, dtype='int16')
         # y = np.frombuffer(y, dtype='float16')
-        self.curve_wav.setData(y)
+        self.curve_wav.setData(y[::int(44100/4)])
 
+        thread_obj2 = threading.Thread(target=self.play)
+        thread_obj2.start()
 
 
     def get_path_feat(self):
-        # self.feat_path = QG.QFileDialog.getOpenFileName(self, 'Get Feature File', '/home/')
-        self.feat_path = '../data/sample.pkl'
+        self.feat_path = QG.QFileDialog.getOpenFileName(self, 'Get Feature File', '/home/')
+        # self.feat_path = '../data/sample.pkl'
         self.le1.setText(self.feat_path)
         with open(self.feat_path, mode='rb') as f:
             self.feat = pickle.load(f)
@@ -169,6 +201,11 @@ class audio_check(QG.QMainWindow):
         for idx in range(10):
             self.curve_featV[idx].setData(self.feat[:, idx])
 
+    def infline_changed(self):
+        infline = self.sender()
+        pos = infline.pos()[0]
+        # print(pos)
+        self.infline_pos = int(pos)
 
 def main():
     app = QG.QApplication(sys.argv)
